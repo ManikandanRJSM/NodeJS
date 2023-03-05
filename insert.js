0b7b906e93854522c096a147b1aff8eb4f63069c
Manikandan <42240958+ManikandanRJSM@users.noreply.github.com>
2023-03-05 14:11:26 +0530

    insert js updated
    insert js updated

--- insert.js
+++ insert.js
@@ -1,8 +1,9 @@
 const connection = require('./connection');
 
-if(connection != 'true'){
-    console.error(connection);
-}
+connection.connect( (error) => {
+    if(error) throw error;return false;
+    
+});
 var sql = 'INSERT INTO users (name, email, password, ocuupation) VALUE ("mani", "mani@gmail.com", "12345", "Dodejs Developer")'
 connection.query(sql, (error, result) => {
     if(error){
