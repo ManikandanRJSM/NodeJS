c11f6b1539395c2dee5b1ccad0d3178443893072
Manikandan <42240958+ManikandanRJSM@users.noreply.github.com>
2023-03-05 13:42:48 +0530

    select user data and all recods
    select user data and all recods

---
+++ select.js
@@ -0,0 +1,46 @@
+const connection = require('./connection');
+
+if(connection != 'true'){
+    console.error(connection);
+}
+function getAllUser(){
+    var sql = 'SELECT *FROM users'
+    connection.query(sql, (error, results, feilds) => {
+        const userdatas = [];
+        if(error){
+            throw error;
+            return false;
+        }
+        for(result of results){
+            userdatas.push({
+                id          : result.id,
+                name        : result.name,
+                email       : result.email,
+                occupation  : result.ocuupation,
+            });
+        }
+        console.log(userdatas);
+    })
+}
+function getUser(id){
+    var sql = `SELECT *FROM users where id = ${id}`;
+    connection.query(sql, (error, result, feilds) => {
+        const userdata = [];
+        if(error){
+            throw error;
+            return false;
+        }
+        for(user of result){
+            userdata.push({
+                id          : user.id,
+                name        : user.name,
+                email       : user.email,
+                occupation  : user.ocuupation,
+            });
+        }
+        console.log(userdata);
+    })
+}
+getAllUser();
+
+getUser(1);
